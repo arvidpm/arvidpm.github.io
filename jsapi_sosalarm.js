d408beab07ab63a9b2e791710f3823325380618c
Arvid Persson Moosavi <arvid.perssonmoosavi@soprasteria.com>
2020-02-27 14:21:22 +0100

    Only pop screens for specified agents

--- jsapi_sosalarm.js
+++ jsapi_sosalarm.js
@@ -1,38 +1,38 @@
 (() => {
 
-    alert("Script init");
-    
     //code added here runs when agent logs in
     ACE.JSApi.onScreenPop((popMsg) => {
 
-        alert("onScreenPop event!");
-      
         console.log('Screen pop event', popMsg);
         
-        if (popMsg.popEvent == 'afterNormalAccept') {
+        if (popMsg.contact.contactData.agentName == 'ulk' ||
+        popMsg.contact.contactData.agentName == 'tekj' ||
+        popMsg.contact.contactData.agentName == 'TeliaTest') {
     
-            console.log('afterNormalAccept screenpop');
-            console.log('A-number:' + popMsg.contact.contactData.ani);
-    
-            //this opens an URL in a custom card inside ACE Interact, men det är beroende av inställningar på sidan som inkluderas (X-Frame-Options)
-            ACE.JSApi.openCustomContent({
-                url: 'https://www.sosalarm.se',
-                title: 'SOS Alarm',
-                customCardId: 'web1'
-            });
-
-            var callingNumber = popMsg.contact.contactData.ani;
-            var applicationUrl = 'https://crmtest.sosalarm.local:8017/new-incoming-call?from='; // will need to fix hard coded url
-            var applicationEntryPoint = applicationUrl + callingNumber;
-
-            //this opens an URL in a new browser window, usually in a new tab but it depends on browser settings
-            window.open(applicationEntryPoint);
-        }
+            if (popMsg.popEvent == 'afterNormalAccept') {
         
-        //agent can manually trigger events
-        else if (popMsg.popEvent == 'manual') {
-            console.log('manual screenpop');
-            alert("Manual screenpop!")
+                console.log('afterNormalAccept screenpop');
+                console.log('A-number:' + popMsg.contact.contactData.ani);
+        
+                //this opens an URL in a custom card inside ACE Interact, men det är beroende av inställningar på sidan som inkluderas (X-Frame-Options)
+                // ACE.JSApi.openCustomContent({
+                //     url: 'https://www.sosalarm.se',
+                //     title: 'SOS Alarm',
+                //     customCardId: 'web1'
+                // });
+
+                var callingNumber = popMsg.contact.contactData.ani;
+                var applicationUrl = 'https://crmtest.sosalarm.local:8017/new-incoming-call?from='; // will need to fix hard coded url
+                var applicationEntryPoint = applicationUrl + callingNumber;
+
+                //this opens an URL in a new browser window, usually in a new tab but it depends on browser settings
+                window.open(applicationEntryPoint);
+            }
+            
+            //agent can manually trigger events
+            else if (popMsg.popEvent == 'manual') {
+                console.log('manual screenpop');
+            }
         }
     }); 
 })();
