97a04ea7a3a5f1a0c440464257e7674e2cd98af5
Arvid Persson Moosavi <arvid.perssonmoosavi@soprasteria.com>
2020-03-12 14:34:27 +0100

    Rearranged to check for null contactData

--- jsapi_sosalarm.js
+++ jsapi_sosalarm.js
@@ -5,42 +5,38 @@
 
         console.log('Screen pop event', popMsg);
 
-        var callingNumber = popMsg.contact.contactData.Ani;
-        var agentName = popMsg.contact.contactData.agentName;
         var allowedAgents = ["tekj", "hkrr"];
+        var popEvent = popMsg.popEvent;
 
-        console.log('Calling number: ' + callingNumber);
-        
-        if (allowedAgents.includes(agentName)) {
+        if (popMsg.contact.contactData != null) {
+            var callingNumber = popMsg.contact.contactData.Ani;
+            var agentName = popMsg.contact.contactData.agentName;
 
-            var applicationUrl = 'https://crmutv.sosalarm.local:8017/new-incoming-call?from='; // will need to fix hard coded url
-            var applicationEntryPoint = applicationUrl + callingNumber;
+            if (allowedAgents.includes(agentName)) {
 
-            window.open(applicationEntryPoint);
+                var applicationUrl = 'https://crmutv.sosalarm.local:8017/new-incoming-call?from='; // will need to fix hard coded url
+                var applicationEntryPoint = applicationUrl + callingNumber;
     
-            if (popMsg.popEvent == 'afterNormalAccept') {
+                window.open(applicationEntryPoint);
         
-                console.log('afterNormalAccept screenpop');
-                console.log('A-number:' + callingNumber);
-        
-                //this opens an URL in a custom card inside ACE Interact, men det är beroende av inställningar på sidan som inkluderas (X-Frame-Options)
-                // ACE.JSApi.openCustomContent({
-                //     url: 'https://www.sosalarm.se',
-                //     title: 'SOS Alarm',
-                //     customCardId: 'web1'
-                // });
-
-                // var callingNumber = popMsg.contact.contactData.Ani;
-                // var applicationUrl = 'https://crmtest.sosalarm.local:8017/new-incoming-call?from='; // will need to fix hard coded url
-                // var applicationEntryPoint = applicationUrl + callingNumber;
-
-                // //this opens an URL in a new browser window, usually in a new tab but it depends on browser settings
-                // window.open(applicationEntryPoint);
-            }
+                if (popEvent === 'afterNormalAccept') {
             
-            //agent can manually trigger events
-            else if (popMsg.popEvent == 'manual') {
-                console.log('manual screenpop');
+                    console.log('afterNormalAccept screenpop');
+            
+                    //this opens an URL in a custom card inside ACE Interact, men det är beroende av inställningar på sidan som inkluderas (X-Frame-Options)
+                    // ACE.JSApi.openCustomContent({
+                    //     url: 'https://www.sosalarm.se',
+                    //     title: 'SOS Alarm',
+                    //     customCardId: 'web1'
+                    // });
+                    
+                    
+                }
+                
+                //agent can manually trigger events
+                else if (popMsg.popEvent == 'manual') {
+                    console.log('manual screenpop');
+                }
             }
         }
     }); 
