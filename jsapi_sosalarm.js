b584b640cc88b929f8c58744b38f49c8dc714790
Arvid Persson Moosavi <arvid.perssonmoosavi@soprasteria.com>
2020-03-13 09:25:01 +0100

    Arrays for all environments that agents can be added in

--- jsapi_sosalarm.js
+++ jsapi_sosalarm.js
@@ -1,43 +1,51 @@
 (() => {
 
-    //code added here runs when agent logs in
+    // Code added here runs when agent logs in
     ACE.JSApi.onScreenPop((popMsg) => {
 
         console.log('Screen pop event', popMsg);
 
-        var allowedAgents = ["tekj", "hkrr"];
+        // Agents can only be placed in one group at a time
+        var allowedAgentsUtv = ["tekj", "hkrr"];
+        var allowedAgentsTest = [];
+        var allowedAgentsProd = [];
+
+        var applicationUrlUtv = 'https://crmutv.sosalarm.local:8017/new-incoming-call?from=';
+        var applicationUrlTest = 'https://crmtest.sosalarm.local:8017/new-incoming-call?from=';
+        var applicationUrlProd = 'https://crm.sosalarm.local:8017/new-incoming-call?from=';
+
         var popEvent = popMsg.popEvent;
         var contact = popMsg.contact;
 
         if (contact != null && contact.contactData != null) {
+
             var callingNumber = popMsg.contact.contactData.Ani;
             var agentName = popMsg.contact.contactData.agentName;
 
-            if (allowedAgents.includes(agentName)) {
+            if (allowedAgentsUtv.includes(agentName)) {
+                window.open(applicationUrlUtv + callingNumber);
+            }
+            else if (allowedAgentsTest.includes(agentName)){
+                window.open(applicationUrlTest + callingNumber);
+            }
+            else if (allowedAgentsProd.includes(agentName)){
+                window.open(applicationUrlProd + callingNumber);
+            }
 
-                var applicationUrl = 'https://crmutv.sosalarm.local:8017/new-incoming-call?from='; // will need to fix hard coded url
-                var applicationEntryPoint = applicationUrl + callingNumber;
-    
-                window.open(applicationEntryPoint);
+            if (popEvent === 'afterNormalAccept') {
+            
+                console.log('afterNormalAccept screenpop');
         
-                if (popEvent === 'afterNormalAccept') {
-            
-                    console.log('afterNormalAccept screenpop');
-            
-                    //this opens an URL in a custom card inside ACE Interact, men det är beroende av inställningar på sidan som inkluderas (X-Frame-Options)
-                    // ACE.JSApi.openCustomContent({
-                    //     url: 'https://www.sosalarm.se',
-                    //     title: 'SOS Alarm',
-                    //     customCardId: 'web1'
-                    // });
-
-                    
-                }
-                
-                //agent can manually trigger events
-                else if (popMsg.popEvent == 'manual') {
-                    console.log('manual screenpop');
-                }
+                // This opens an URL in a custom card inside ACE Interact, men det är beroende av inställningar på sidan som inkluderas (X-Frame-Options)
+                // ACE.JSApi.openCustomContent({
+                //     url: 'https://www.sosalarm.se',
+                //     title: 'SOS Alarm',
+                //     customCardId: 'web1'
+                // });
+            }
+            // Agent can manually trigger events
+            else if (popMsg.popEvent == 'manual') {
+                console.log('manual screenpop');
             }
         }
     }); 
